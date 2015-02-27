642851bd7d4e08bef3004a8735a08c0560052a1a
LLVM Packaging Team <pkg-llvm-team@lists.alioth.debian.org>
2015-02-27 10:26:38 +0000

    clang-format-version
    ===================================================================
    
    Gbp-Pq: Name clang-format-version.diff

--- clang/tools/clang-format/clang-format.el
+++ clang/tools/clang-format/clang-format.el
@@ -13,7 +13,7 @@
 
 ;; *Location of the clang-format binary. If it is on your PATH, a full path name
 ;; need not be specified.
-(defvar clang-format-binary "clang-format")
+(defvar clang-format-binary "clang-format-3.5")
 
 (defun clang-format-region ()
   "Use clang-format to format the currently active region."
