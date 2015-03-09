64f53e5b43a39239ba580ac90eef2a5f24ca91ba
LLVM Packaging Team <pkg-llvm-team@lists.alioth.debian.org>
2015-03-09 01:09:12 +0000

    fix-llvm-config-obj-src-root
    ===================================================================
    
    Gbp-Pq: Name fix-llvm-config-obj-src-root.patch

--- tools/llvm-config/llvm-config.cpp
+++ tools/llvm-config/llvm-config.cpp
@@ -322,9 +322,9 @@ int main(int argc, char **argv) {
         OS << "ON\n";
 #endif
       } else if (Arg == "--obj-root") {
-        OS << ActivePrefix << '\n';
+        OS << ActivePrefix << "/build/" << '\n';
       } else if (Arg == "--src-root") {
-        OS << LLVM_SRC_ROOT << '\n';
+        OS << ActivePrefix << "/build/" << '\n';
       } else {
         usage();
       }
