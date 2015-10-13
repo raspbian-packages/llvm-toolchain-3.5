9da4f08194ed1e6652da0a7839e6b015febda065
LLVM Packaging Team <pkg-llvm-team@lists.alioth.debian.org>
2015-10-13 07:55:31 +0000

    python-clangpath
    ===================================================================
    
    Gbp-Pq: Name python-clangpath.diff

--- clang/bindings/python/clang/cindex.py
+++ clang/bindings/python/clang/cindex.py
@@ -3443,7 +3443,7 @@ class Config:
         elif name == 'Windows':
             file = 'libclang.dll'
         else:
-            file = 'libclang.so'
+            file = 'libclang-3.5.so'
 
         if Config.library_path:
             file = Config.library_path + '/' + file
