70ea22f3fd3178dde38e62cdde379827ed6bb293
LLVM Packaging Team <pkg-llvm-team@lists.alioth.debian.org>
2016-10-19 11:47:22 +0000

    fix-ftbfs-ocaml-4.02
    Gbp-Pq: Name fix-ftbfs-ocaml-4.02

--- test/Bindings/Ocaml/executionengine.ml
+++ test/Bindings/Ocaml/executionengine.ml
@@ -1,7 +1,7 @@
 (* RUN: rm -rf %t.builddir
  * RUN: mkdir -p %t.builddir
  * RUN: cp %s %t.builddir
- * RUN: %ocamlopt -warn-error A llvm.cmxa llvm_target.cmxa llvm_executionengine.cmxa %t.builddir/executionengine.ml -o %t
+ * RUN: %ocamlopt  llvm.cmxa llvm_target.cmxa llvm_executionengine.cmxa %t.builddir/executionengine.ml -o %t
  * RUN: %t
  * XFAIL: vg_leak
  *)
